f9bcdb30843b6adf2f5bc1ff7cc9f468fdf72861
LivenChief <liven_zhan@chiefgroup.com.hk>
2023-10-11 16:32:26 +0800

    Comment FileHepler code

--- Templates/Util/FileHelper.swift
+++ Templates/Util/FileHelper.swift
@@ -12,6 +12,7 @@ class FileHelper {
     static let shared = FileHelper()
     private init() {
         // 初始化时设置默认路径
+        // Mac 文件访问权限官方文档：https://developer.apple.com/documentation/security/app_sandbox/accessing_files_from_the_macos_app_sandbox
         let sandboxDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         defaultPath = sandboxDirectory.appendingPathComponent("templates").path
         
@@ -28,6 +29,10 @@ class FileHelper {
     let fileManager = FileManager.default
     var defaultPath: String
     
+    
+    /// 指定路径下读取文件夹
+    /// - Parameter path: path
+    /// - Returns: [FolderName]
     func folders(atPath path: String? = nil) -> [String]? {
         let fullPath = path ?? defaultPath
         do {
@@ -47,6 +52,12 @@ class FileHelper {
         }
     }
     
+    
+    /// 指定路径下所有的文件（包括文件夹），如果传后缀则只会筛选出后缀名一致的文件
+    /// - Parameters:
+    ///   - directoryPath: path
+    ///   - fileExtension: 文件后缀
+    /// - Returns: [FileName]
     func files(inDirectory directoryPath: String, withExtension fileExtension: String? = nil) -> [String]? {
         do {
             let directoryURL = URL(fileURLWithPath: directoryPath)
@@ -66,10 +77,20 @@ class FileHelper {
         }
     }
     
+    
+    /// 判断指定路径下的文件是否存在
+    /// - Parameter path: path
+    /// - Returns: isExists
     func fileExists(atPath path: String) -> Bool {
         return fileManager.fileExists(atPath: path)
     }
     
+    
+    /// 重命名或移动文件
+    /// - Parameters:
+    ///   - path: path
+    ///   - newPath: path
+    /// - Returns: 是否成功
     func renameFile(atPath path: String, to newPath: String) -> Bool {
         do {
             try fileManager.moveItem(atPath: path, toPath: newPath)
@@ -80,6 +101,10 @@ class FileHelper {
         }
     }
     
+    
+    /// 读取文件的基本信息
+    /// - Parameter path: path
+    /// - Returns: FileAttributeKey
     func fileInfo(atPath path: String) -> [FileAttributeKey: Any]? {
         do {
             let attributes = try fileManager.attributesOfItem(atPath: path)
@@ -90,14 +115,27 @@ class FileHelper {
         }
     }
     
+    
+    /// 读取指定路径的文件内容
+    /// - Parameter path: path
+    /// - Returns: file content
     func readFileContent(atPath path: String) -> Data? {
         return fileManager.contents(atPath: path)
     }
     
+    
+    /// 创建文件并填充content
+    /// - Parameters:
+    ///   - path: path
+    ///   - content: content
+    /// - Returns: 是否成功
     func createFile(atPath path: String, content: Data? = nil) -> Bool {
         return fileManager.createFile(atPath: path, contents: content, attributes: nil)
     }
     
+    /// <#Description#>
+    /// - Parameter path: <#path description#>
+    /// - Returns: <#description#>
     func deleteFile(atPath path: String) -> Bool {
         do {
             try fileManager.removeItem(atPath: path)
@@ -108,6 +146,11 @@ class FileHelper {
         }
     }
     
+    /// 重命名或移动文件
+    /// - Parameters:
+    ///   - path: path
+    ///   - newPath: path
+    /// - Returns: 是否成功
     func moveFile(fromPath path: String, toPath newPath: String) -> Bool {
         do {
             try fileManager.moveItem(atPath: path, toPath: newPath)
@@ -120,7 +163,10 @@ class FileHelper {
 }
 
 extension FileHelper {
+    /// 读取moudle文件夹内的所有文件
+    /// - Parameter moudle: moduleName
+    /// - Returns: [fileName]
     func moudleContainFiles(_ moudle: String) -> [String]? {
-        files(inDirectory: defaultPath + "templates")
+        files(inDirectory: defaultPath + "templates/" + moudle)
     }
 }
