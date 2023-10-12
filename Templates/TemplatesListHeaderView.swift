5e0814a7fabcfb31aa9fbf7b40e0c6cf386526e4
LivenChief <liven_zhan@chiefgroup.com.hk>
2023-10-12 10:37:27 +0800

    update add global params setting logic

--- Templates/TemplatesListHeaderView.swift
+++ Templates/TemplatesListHeaderView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Item: Identifiable {
     let id = UUID()
-    let key: String
+    var key: String
     var value: String
 }
 
@@ -18,7 +18,7 @@ struct TemplatesListHeaderView: View {
 
     var body: some View {
         VStack(spacing: 0) {
-            sectionHeaderView()
+            SectionHeaderView()
             List {
                 ForEach(items.indices, id: \.self) { index in
                     ItemRow(item: $items[index])
@@ -29,8 +29,14 @@ struct TemplatesListHeaderView: View {
         .padding(.vertical, 12)
     }
     
+    private func addParamItem() {
+        items.append(Item(key: "", value: ""))
+    }
+}
+
+extension TemplatesListHeaderView {
     @ViewBuilder
-    func sectionHeaderView() -> some View {
+    func SectionHeaderView() -> some View {
         HStack {
             Text("模版全局参数")
                 .font(.title3)
@@ -48,68 +54,65 @@ struct TemplatesListHeaderView: View {
                 }
         }.padding(.bottom, 12)
     }
-    
-    @ViewBuilder
-    func paramItemView(key: String, value: String?) -> some View {
-        VStack(spacing: 6) {
-            HStack {
-                Text("Key:")
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-                Spacer()
-                TextField(key, text: .constant(""))
-                Text("\(key)")
-                    .font(.headline)
-                    .foregroundColor(.primary)
-            }
-            HStack {
-                Text("Value:")
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-                Spacer()
-                Text("\(value ?? "")")
-                    .font(.headline)
-                    .foregroundColor(.primary)
-            }
-            Divider()
-        }
-        .padding(.bottom, 8)
-        .padding(.trailing, 6)
-    }
-    
-    private func addParamItem() {
-        items.append(Item(key: "", value: ""))
-    }
 }
 
 struct ItemRow: View {
     @Binding var item: Item
-    @State private var isEditing = false
+    @State private var isKeyEditing = false
+    @State private var isValueEditing = false
     
     var body: some View {
         VStack(spacing: 6) {
-            HStack {
-                Text("Key:")
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-                Spacer()
-                Text("\(item.key)")
-                    .font(.headline)
-                    .foregroundColor(.primary)
-            }
-            
-            if isEditing {
+            // Key
+            if isKeyEditing {
                 HStack {
-                    Text("Value:")
+                    Text("Key:")
                         .font(.headline)
                         .foregroundColor(.secondary)
-                    FocusedTextField("Enter value", text: $item.value, keepFirstResponder: .constant(false), onCommit: {
-                        isEditing = false
+                        .hSpacing(.leading)
+                        .frame(width: 70)
+                    FocusedTextField("Enter Key", text: $item.key, keepFirstResponder: .constant(false), onCommit: {
+                        isKeyEditing = false
                     })
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .contentShape(Rectangle())
                     .onTapGesture(count: 2) {
-                        isEditing = true
+                        isKeyEditing = true
+                    }
+                }
+            } else {
+                HStack {
+                    Text("Key:")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                        .hSpacing(.leading)
+                        .frame(width: 70)
+                    Spacer()
+                    Text("\(item.key)")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                }
+                .contentShape(Rectangle())
+                .onTapGesture(count: 2) {
+                    isKeyEditing = true
+                }
+            }
+            
+            // Value
+            if isValueEditing {
+                HStack {
+                    Text("Value:")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                        .hSpacing(.leading)
+                        .frame(width: 70)
+                    FocusedTextField("Enter value", text: $item.value, keepFirstResponder: .constant(false), onCommit: {
+                        isValueEditing = false
+                    })
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .contentShape(Rectangle())
+                    .onTapGesture(count: 2) {
+                        isValueEditing = true
                     }
                 }
             } else {
@@ -117,6 +120,8 @@ struct ItemRow: View {
                     Text("Value:")
                         .font(.headline)
                         .foregroundColor(.secondary)
+                        .hSpacing(.leading)
+                        .frame(width: 70)
                     Spacer()
                     Text("\(item.value)")
                         .font(.headline)
@@ -124,7 +129,7 @@ struct ItemRow: View {
                 }
                 .contentShape(Rectangle())
                 .onTapGesture(count: 2) {
-                    isEditing = true
+                    isValueEditing = true
                 }
             }
             Divider()
